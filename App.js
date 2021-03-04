857593cdbe1218236055b4b118ad87a152aa3b09
elemson <toluwani@juststart.dev>
2021-03-04 18:12:22 +0200

    added styles to app.js

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import {StyleSheet, Text, View} from 'react-native';
 import ApiContainer from './App/Screens/ApiContainer';
 
 const App = () => {
-  return (
-    <>
-      <ApiContainer />
-    </>
-  );
+  return <>{<ApiContainer />}</>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
