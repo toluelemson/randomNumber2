671337644eb6af95b8565c2b3eaa8e970959c952
elemson <toluwani@juststart.dev>
2021-03-05 12:32:28 +0200

    render random item

--- App/Screens/ApiView.js
+++ App/Screens/ApiView.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {View, Text, Button, FlatList, ActivityIndicator} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+  SafeAreaView,
+} from 'react-native';
 import styles from './ApiStyles';
 const ApiView = props => {
-  const {
-    getRandom,
-    axiosData,
-    renderItem,
-    FlatListItemSeparator,
-    loading,
-  } = props;
+  const {getRandom, axiosData, FlatListItemSeparator, loading} = props;
+  //   const people = [{name: 'shaun', key: '1'}, {name: 'yahh', key: '2'}];
   return (
     <View style={styles.parentContainer}>
       <View>
@@ -23,19 +26,31 @@ const ApiView = props => {
       </View>
 
       {loading ? (
-        <View style={styles.loader}>
-          <ActivityIndicator size="large" color="#0c9" />
-          <Text style={{fontSize: 16, color: 'red'}}>Loading Data...</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.loader}>
+            <ActivityIndicator size="large" color="#0c9" />
+            <Text style={{fontSize: 16, color: 'red'}}>Loading Data...</Text>
+          </View>
+        </SafeAreaView>
       ) : (
-        <FlatList
-          data={axiosData}
-          ItemSeparatorComponent={FlatListItemSeparator}
-          renderItem={item => renderItem(item.random)}
-          keyExtractor={item => item.toString()}
-        />
+        <SafeAreaView style={styles.list}>
+          <FlatList
+            data={axiosData}
+            ItemSeparatorComponent={FlatListItemSeparator}
+            renderItem={({item}) => (
+              <Text style={styles.list}>
+                <TouchableOpacity style={styles.list}>
+                  <Text style={styles.lightText}>
+                    <h4>{item}</h4>
+                  </Text>
+                </TouchableOpacity>
+              </Text>
+            )}
+          />
+        </SafeAreaView>
       )}
     </View>
   );
 };
+
 export default ApiView;
